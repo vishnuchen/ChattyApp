b6ab3a89700c3ab01da9a36c77ff1c8ccfaa7100
Vishnu Chennuru <vishnuchen@gmail.com>
2019-07-05 18:13:59 +0000

    made changes to fix notification

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,12 +2,20 @@ import React, {Component} from 'react';
 
 class Message extends Component {
   render() {
-    return (
-        <div className="message">
-          <span className="message-username">{this.props.message.username}</span>
-          <span className="message-content">{this.props.message.content}</span>
-        </div>      
-    );
+    if (this.props.message.type === 'incomingMessage') {
+      return (
+          <div className="message">
+            <span className="message-username">{this.props.message.username}</span>
+            <span className="message-content">{this.props.message.content}</span>
+          </div>     
+      );
+    } else {
+      return (
+        <div className="message system">
+          {this.props.message.content}
+        </div>
+      );
+    }
   }
 }
 export default Message;
